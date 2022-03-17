a57dfdb33a51bb839ae7f2645764cf9d7ad7c80a
Alexandru Dumitru Maroz <alexandru.dumitru@estudiantat.upc.edu>
2022-03-17 10:52:20 +0000

    blocked host resolved

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << /[a-z0-9]+\.vfs.cloud9.us-east-1.amazonaws.com/
+  #config.hosts << "c62b336715d94a78bbca35b7c347c3cc.vfs.cloud9.us-east-1.amazonaws.com"
 end
