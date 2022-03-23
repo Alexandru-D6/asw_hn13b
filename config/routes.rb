13e1920b6a41919409d56af3b24380c48e0f629f
Alexandru Dumitru Maroz <alexandru.dumitru@estudiantat.upc.edu>
2022-03-23 16:32:21 +0000

    nothing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :submissions
+  
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   root 'submissions#index'
 end
