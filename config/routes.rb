d49f527e1f7b374a6e79885920e72fc7884a97e9
Alexandru Dumitru <alexandru666@outlook.es>
2022-03-24 12:30:00 +0100

    Revert "migration error"
    This reverts commit 7f5761c5e0b336353f5d5cbe91d0d4e71ef2f516.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :submissions do
     member do
       put 'upvote'
@@ -8,8 +7,6 @@ Rails.application.routes.draw do
   
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   
   get 'news', to: 'submissions#index'
   get 'newest', to: 'submissions#index', newest: 'true'
