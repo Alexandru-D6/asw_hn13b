54ae4e5d4a2484be7dfdac3502ca6e81f793bde5
Alexandru Dumitru Maroz <alexandru.dumitru@estudiantat.upc.edu>
2022-04-12 12:56:38 +0000

    login fullyfunctional

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,13 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+    
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Github'
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session['devise.github_data'] = request.env['omniauth.auth'].except('extra')
+      redirect_to new_userregistration_url, alert: @user.errors.full_messages.join('\n')
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,26 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :validatable, :omniauthable
          
   def self.from_omniauth(auth)
+    
+    data = auth.info
+    user = User.where(email: data['email'])
+    
+    unless user
+      user = User.create(
+        provider: data['provider'],
+        name: data['name'],
+        username: data['nickname'],
+        uid: data['uid'],
+        email: data['email'],
+        password: Devise.friendly_token[0,20]
+        )
+    end
+    
+    
+    
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user |
       user.provider = auth.provider
       user.name = auth.info.name
