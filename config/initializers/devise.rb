67aab287542e3de4c7ca61cecbb6b7d08f53ff84
Alvaro <alvaro.rodriguez.rubio@estudiantat.upc.edu>
2022-04-20 16:05:44 +0000

    canvis

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, '############', '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@', scope: 'user'
+  config.omniauth :github, '0c3a3a6596e04d7aa136', '3cd4da0f29a1a10cf274168aeb08aaaf9da10b59', scope: 'user'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
