f8a5045db273a5c5d99d34f3e46c43949ffa9eb6
Alexandru Dumitru Maroz <alexandru.dumitru@estudiantat.upc.edu>
2022-04-21 01:16:12 +0000

    Heroku deployed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   #serialize :id_sons
   
   has_many :comments
+  
+  validates :comment, length: { minimum: 5, maximum: 256}, presence: {message: "Comment can't be blank."}
 end
